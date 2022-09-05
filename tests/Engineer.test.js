32c6346be9228743cfbc2bf8186124859cca1395
Sabrina LeMay <b33na1997@gmail.com>
2022-09-05 12:28:54 -0700

    updated engineer.test

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,25 +1,20 @@
 const Engineer = require('./lib/Engineer');
-const engineer = new Engineer('sabrina','12345','my@email.com','myGithub');
 
-test('testing to see if we can get engineer object', () =>{
-    expect(engineer.name).toBe('sabrina');
-    expect(engineer.id).toBe('12345');
-    expect(engineer.email).toBe('my@email.com');
-    expect(engineer.school).toBe('myGithub');
+test('testing to see if github account comes from constructor', () =>{
+    const githubTest = "GitHubName";
+    const engineer = new Engineer("foo", 1, "test@email.com", githubTest);
+    expect(engineer.github).toBe(githubTest);
 });
 
-test('testing to see if name comes from getName()', () =>{
-    expect(engineer.getName()).toBe('sabrina');
+
+test('Does getRole() return Engineer', () =>{
+    const engineerTest = "Engineer";
+    const engineer = new Engineer("foo", 1, "test@email.com", "GitHubName");
+    expect(engineer.getRole()).toBe(engineerTest);
 });
 
-test('testing to see if id comes from getId()', () =>{
-    expect(engineer.getId()).toBe('12345');
-});
-
-test('testing to see if email comes from getEmail()', () =>{
-    expect(engineer.getEmail()).toBe('my@email.com');
-});
-
-test('testing to see if github comes from getGithub', () =>{
-    expect(engineer.getSchool()).toBe('myGithub');
+test('Does getGithub() return GitHub username', () =>{
+    const githubTest = "GitHubName";
+    const engineer = new Engineer("foo", 1, "test@email.com", githubTest);
+    expect(engineer.getGithub()).toBe(githubTest);
 });
